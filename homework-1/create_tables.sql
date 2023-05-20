6822c2aa0a267d29147eedaf5f8f21ee190bd92a
Татьяна <tanechka201286grudeva@gmail.com>
2023-05-20 22:19:12 +0300

    add formula

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,28 @@
--- SQL-команды для создания таблиц
+--SQL-команды для создания таблиц
+
+CREATE TABLE customers_data
+(
+	id int PRIMARY KEY,
+	customer_id	varchar(50),
+	company_name varchar(50),
+	contact_name varchar(100)
+)
+
+CREATE TABLE employees_data
+(
+	employees_id int PRIMARY KEY,
+	first_name	varchar(50),
+	last_name varchar(50),
+	title varchar(100),
+	birth_date date,
+	notes varchar(10000)
+)
+
+CREATE TABLE orders_data
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(50),
+	employee_id int,
+	order_date date,
+	ship_city varchar(50)
+)
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,71 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import psycopg2
+import csv
+
+file_1 = "customers_data.csv"
+file_2 = "employees_data.csv"
+file_3 = "orders_data.csv"
+
+
+def read_customers():
+    """Считывает данные из customers_data.csv"""
+    dic = []
+    with open(file_1, 'r', encoding='utr-8') as file:
+        file_read = csv.reader(file, delimiter=', ')
+        for line in file_read:
+            dic.append(line)
+
+
+def read_employees_data():
+    """Считывает данные из employees_data.csv"""
+    dic = []
+    with open(file_2, 'r', encoding='utr-8') as file:
+        file_read = csv.reader(file, delimiter=', ')
+        for line in file_read:
+            dic.append(line)
+
+
+def read_orders_data():
+    """Считывает данные из orders_data.csv"""
+    dic = []
+    with open(file_3, 'r', encoding='utr-8') as file:
+        file_read = csv.reader(file, delimiter=', ')
+        for line in file_read:
+            dic.append(line)
+
+
+def main():
+    """Заполнит созданные таблицы данными"""
+
+    conn = psycopg2.connect(
+        host='localhost',
+        database='north',
+        user='postgres',
+        password="Zima-2023"
+    )
+
+    data = read_customers()
+    with conn:
+        cursor = conn.cursor()
+        for item in data[1:]:
+            cursor.execute(f"INSERT INTO customers_data (customer_id, company_name, contact_name)"
+                           f" VALUES ('{item[1]}', '{item[2]}', '{item[3]}')")
+
+    data_2 = read_employees_data()
+    with conn:
+        cursor = conn.cursor()
+        for item in data_2[1:]:
+            cursor.execute(f"INSERT INTO customers_data (first_name, last_name, title, birth_date, notes)"
+                           f" VALUES ('{item[1]}', '{item[2]}', '{item[3]}','{item[4]}', '{item[5]}')")
+
+    data_3 = read_orders_data()
+    with conn:
+        cursor = conn.cursor()
+        for item in data_3[1:]:
+            cursor.execute(f"INSERT INTO customers_data (order_id, customer_id, employee_id, order_date,ship_city)"
+                           f" VALUES ('{item[1]}', '{item[2]}', '{item[3]}','{item[4]}', '{item[5]}')")
+
+
+if __name__ == 'main':
+    main()
